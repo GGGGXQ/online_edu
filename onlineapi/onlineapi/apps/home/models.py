c95434a4188b3997458e86c3d33e52fa3651131f
GGGGXQ <19860599289@163.com>
2025-04-21 17:19:28 +0800

    feature: 在Admin站点中管理公共数据与用户数据

--- onlineapi/onlineapi/apps/home/models.py
+++ onlineapi/onlineapi/apps/home/models.py
@@ -1,4 +1,5 @@
 from models import models, BaseModel
+from django.utils.html import format_html
 
 
 # Create your models here.
@@ -21,11 +22,20 @@ class Nav(BaseModel):
 
 class Banner(BaseModel):
     image = models.ImageField(upload_to="banner/%Y/", verbose_name="图片地址")
-    link = models.CharField(max_length=500, verbose_name="连接")
-    note = models.CharField(max_length=150, verbose_name="链接地址")
+    link = models.CharField(max_length=500, verbose_name="链接地址")
+    note = models.CharField(max_length=150, verbose_name="备注信息")
     is_http = models.BooleanField(default=False, verbose_name="是否是外部链接", help_text="站点链接地址：http://www.baidu.com/book<br>站点链接地址：/book/")
 
     class Meta:
         db_table = "ol_banner"
         verbose_name = "轮播广告"
         verbose_name_plural = verbose_name
+
+    def image_html(self):
+        if self.image:
+            return format_html(f'<img style="border-radius: 0%;max-height: 100px; max-width: 400px;" src="{self.image.url}">')
+        return ""
+
+    image_html.short_description = "广告图片"
+    image_html.allow_tags = True
+    image_html.admin_order_field = "image"
