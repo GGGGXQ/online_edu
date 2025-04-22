9b07a74f6e038626265c6bbd1481da97f8888d60
GGGGXQ <19860599289@163.com>
2025-04-22 10:49:12 +0800

    feature: 根据不同的学习方向与课程分类，展示课程列表信息

--- onlineweb/src/api/course.js
+++ onlineweb/src/api/course.js
@@ -1,20 +1,25 @@
 import http from "../utils/http";
-import {reactive, ref} from "vue";
+import {reactive, ref} from "vue"
+
 
 const course = reactive({
     current_direction: 0,  // 当前选中的学习方向，0表示所有方向
     current_category: 0,  // 当前选中的课程分类，0表示不限分类
     direction_list: [],    // 学习方向列表
     category_list: [],    // 课程分类列表
-    // 获取学习方向信息
+    course_list: [],       // 课程列表数据
     get_course_direction(){
+        // 获取学习方向信息
         return http.get("/courses/directions/")
     },
-
-    // 获取课程分类信息
     get_course_category () {
-      return http.get(`/courses/categories/${this.current_direction}/`)
+        // 获取课程分类信息
+        return http.get(`/courses/categories/${this.current_direction}/`)
+    },
+    get_course_list () {
+        // 获取课程列表信息
+      return http.get(`/courses/${this.current_direction}/${this.current_category}/`)
     }
-});
+})
 
 export default course;
