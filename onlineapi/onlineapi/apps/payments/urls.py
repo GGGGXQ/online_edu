cbb2d2b5b035a7a1c73b13232252a18236ef8b3c
GGGGXQ <19860599289@163.com>
2025-05-04 21:35:22 +0800

    feature: 服务端接收并处理支付宝异步通知结果

--- onlineapi/onlineapi/apps/payments/urls.py
+++ onlineapi/onlineapi/apps/payments/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     re_path("^alipay/(?P<order_number>[0-9]+)/$", views.AlipayAPIViewSet.as_view({"get": "link"})),
     path("alipay/result/", views.AlipayAPIViewSet.as_view({"get": "return_result"})),
-    re_path("^aplipay/query/(?P<order_number>[0-9]+)/$", views.AlipayAPIViewSet.as_view({"get": "query"}))
+    re_path("^aplipay/query/(?P<order_number>[0-9]+)/$", views.AlipayAPIViewSet.as_view({"get": "query"})),
+    path("alipay/notify", views.AlipayAPIViewSet.as_view({"post": "notify_result"})),
 ]
