3efd5b20b09f7adc44c0687c80ec71ecf9531a99
GGGGXQ <19860599289@163.com>
2025-05-04 10:40:14 +0800

    feature: 服务端接收客户端转发的同步支付结果并验证修改订单状态

---
+++ onlineapi/onlineapi/utils/alipaysdk.py
@@ -0,0 +1,98 @@
+from django.conf import settings
+from alipay import AliPay
+from alipay.utils import AliPayConfig
+from datetime import datetime
+
+
+class AliPaySDK(AliPay):
+    """支付宝接口sdk工具类"""
+
+    def __init__(self, config=None):
+        if config is None:
+            self.config = settings.ALIPAY
+        else:
+            self.config = config
+
+        app_private_key_string = open(self.config["app_private_key_path"]).read()
+        alipay_public_key_string = open(self.config["alipay_public_key_path"]).read()
+        super().__init__(
+            appid=self.config["appid"],
+            app_notify_url=self.config["notify_url"],
+            app_private_key_string=app_private_key_string,
+            alipay_public_key_string=alipay_public_key_string,
+            sign_type=self.config["sign_type"],
+            debug=self.config["debug"],
+            config=AliPayConfig(timeout=self.config["timeout"])
+        )
+
+    def page_pay(self, order_number, real_price, order_name):
+        """
+        生成支付链接
+        @params order_number: 商户订单号
+        @params real_price: 订单金额
+        @params order_name: 订单标题
+        @return 支付链接
+        """
+        order_string = self.client_api(
+            "alipay.trade.page.pay",
+            biz_content={
+                "out_trade_no": order_number,  # 订单号
+                "total_amount": real_price,  # 订单金额
+                "subject": order_name,  # 订单标题
+                "product_code": "FAST_INSTANT_TRADE_PAY"  # 支付类型
+            },
+            return_url=self.config["return_url"],  # 可选，同步回调地址，必须填写客户端地址
+            notify_url=self.config["notify_url"]  # 可选，不填则采用全局默认notify_url，必须填写服务端的路径
+        )
+        return f"{self.config['gateway']}?{order_string}"
+
+    def check_sign(self, data):
+        """
+        验证返回的支付结果中的签证信息
+        @params data: 支付平台返回的支付结果，字典格式
+        """
+        signature = data.pop('sign')
+        success = self.verify(data, signature)
+        return success
+
+    def query(self, order_number):
+        """
+        根据订单号查询订单状态
+        @params order_number: 订单号
+        """
+        return self.server_api(
+            "alipay.trade.query",
+            biz_content={
+                "out_trade_no": order_number
+            }
+        )
+
+    def refund(self, order_number, real_price):
+        """
+        原路退款
+        @params order_number: 退款的订单号
+        @params real_price: 退款的订单金额
+        """
+        self.server_api(
+            "alipay.rade.refund",
+            biz_content={
+                "out_trade_no": order_number,
+                "refund_amount": real_price
+            }
+        )
+
+    def transfer(self, account, amount):
+        """
+        转账给个人
+        @params account: 收款人的支付宝账号
+        @params amount: 转账金额
+        """
+        return self.server_api(
+            "alipay.fund.trans.toaccount.transfer",
+            biz_content={
+                "out_biz_no": datetime.now().strftime("%Y%m%d%H%M%S"),
+                "payee_type": "ALIPAY_LOGONID/ALIPAY_USERID",
+                "payee_account": account,
+                "amount": amount
+            }
+        )
